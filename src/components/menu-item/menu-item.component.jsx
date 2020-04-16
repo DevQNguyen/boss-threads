3da92be701b113e9eaa8f2079c62629eb2c59449
DevQNguyen <go.quang.nguyen@gmail.com>
2020-04-16 15:56:04 -0700

    Finished styling menu item for hover, scaling, and transition effects

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import "./menu-item.styles.scss";
 
 const MenuItem = (props) => (
-  <div
-    className={`${props.size} menu-item`}
-    style={{ backgroundImage: `url(${props.imageUrl}` }}
-  >
+  <div className={`${props.size} menu-item`}>
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${props.imageUrl})` }}
+    />
     <div className="content">
       <h1 className="title">{props.title.toUpperCase()}</h1>
-      <a href={props.linkUrl} className="subtitle">
-        SHOP NOW
-      </a>
+      <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
