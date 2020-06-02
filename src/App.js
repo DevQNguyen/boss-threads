9787b93de3b34e68c6e2817d8743161b0c25590a
DevQNguyen <go.quang.nguyen@gmail.com>
2020-06-02 00:31:53 -0700

    Implemented Redirect to root route when user is signed in, refactored user type string to constant.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import Header from './components/header/header.component';
@@ -14,21 +14,24 @@ class App extends React.Component {
 
 	componentDidMount() {
 		const { setCurrentUser } = this.props;
-		// onAuthStateChanged Listener returns a firebase.Unsubscribe method
+		// onAuthStateChanged Listener, creates a user auth object
+		// and returns a firebase.Unsubscribe method
 		this.unSubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
 			if (userAuth) {
-				//
+				// Create a user profile with user auth object
 				const userRef = await createUserProfileDocument(userAuth);
 
 				// Listener: If there's a change in snapShop, grab the current snapShot
+				// and update state by firing an Action Creator, setCurrentUser
 				userRef.onSnapshot((snapShot) => {
+					// Pass in snapShot data to Action Creator
 					setCurrentUser({
 						id: snapShot.id,
 						...snapShot.data()
 					});
 				});
 			} else {
-				// If userAuth is NULL, return userAuth to
+				// If userAuth is NULL, set currentUser to null
 				setCurrentUser(userAuth);
 			}
 		});
@@ -47,16 +50,30 @@ class App extends React.Component {
 				<Switch>
 					<Route exact path='/' component={HomePage} />
 					<Route path='/shop' component={ShopPage} />
-					<Route path='/signin' component={SignInRegisterPage} />
+					<Route
+						exact
+						path='/signin'
+						render={() =>
+							this.props.currentUser ? (
+								<Redirect to='/' />
+							) : (
+								<SignInRegisterPage />
+							)
+						}
+					/>
 				</Switch>
 			</div>
 		);
 	}
 }
 
+const mapStateToProps = ({ user }) => ({
+	currentUser: user.currentUser
+});
+
 // Dispatch an Action Creator object to all Reducers
 const mapDispatchToProps = (dispatch) => ({
 	setCurrentUser: (user) => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
